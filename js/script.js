568af09dd899faa6774d83e87a18994756c8cb38
ai-s1014 <aaaip37@gmail.com>
2024-01-23 22:23:46 +0900

    swiper実装

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 // hero swiper
-  const mySwiper = new Swiper('.swiper', {
-    loop: true,
+  var mySwiper = new Swiper('.slider1', {
 	effect: 'fade',
 	fadeEffect: {
-        crossFade: true
+        crossFade: true,
     },
 	autoplay: {
-	  delay: 4000,
+	  delay: 6000,
 	  disableOnInteraction: false,
+	  waitForTransition: false,
 	},
-	speed: 2000,
-	followFinger: false,
+	speed: 0,
+	loopAdditionalSlides: 5,
 	allowTouchMove: false,
+});
+
+var mySwiper = new Swiper('.slider2', {
+	loop: true,
+	loopAdditionalSlides: 1,
+	slidesPerView: 'auto',
+	autoplay: {
+	  delay: 5000,
+	  disableOnInteraction: false,
+	  waitForTransition: false,
+	},
+	speed: 4000,
+	allowTouchMove: false,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	  },
 });
