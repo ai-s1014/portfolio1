ffcdad1062b663fa22a92220c6d7439cb1253453
ai-s1014 <aaaip37@gmail.com>
2024-02-17 22:58:47 +0900

    “修正”

--- js/script.js
+++ js/script.js
@@ -18,21 +18,18 @@ var mySwiper = new Swiper('.slider2', {
 	breakpoints: {
 		0: {
 			slidesPerView: 1,
-			centeredSlides: true,
 		},
 		450: {
 			slidesPerView: 2.5,
-			centeredSlides: true,
 		},
 		741: {
 			slidesPerView: 3.5,
-			centeredSlides: true,
 		},
 		1080: {
 			slidesPerView: 4,
-			centeredSlides: true,
 		},
 	},
+	centeredSlides: true,
 	spaceBetween: 60,
 	speed: 1000,
 	autoplay: {
